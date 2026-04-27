8e568ae683c54928ea5a9531a8ae655587208085
jonym7293-alt <jonym7293@gmail.com>
2026-04-27 18:58:29 +0600

    middleware file updated

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,10 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  const session = request.cookies.get('better-auth.session_token');
+  const sessionToken = 
+    request.cookies.get('better-auth.session_token') || 
+    request.cookies.get('__Secure-better-auth.session_token');
+
   const { pathname, origin } = request.nextUrl;
 
   const protectedRoutes = ['/career', '/news'];
@@ -10,9 +13,8 @@ export function middleware(request) {
     pathname.startsWith(route),
   );
 
-  if (!session && isProtectedRoute) {
+  if (!sessionToken && isProtectedRoute) {
     const callbackUrl = encodeURIComponent(pathname);
-
     return NextResponse.redirect(
       new URL(`/login?callbackUrl=${callbackUrl}`, origin),
     );
