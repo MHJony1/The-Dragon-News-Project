c750cd237a3bfa69c05a6ff04a5bc1579cbac39e
jonym7293-alt <jonym7293@gmail.com>
2026-04-27 20:10:39 +0600

    added righsidebar section images and styles

--- src/components/homepage/news/RighSidebar.jsx
+++ src/components/homepage/news/RighSidebar.jsx
@@ -1,7 +1,15 @@
 'use client';
 import { authClient } from '@/lib/auth-client';
+import Image from 'next/image';
 import React from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
+import fbIcon from '@/assets/fb.png';
+import twitterIcon from '@/assets/twitter.png';
+import instaIcon from '@/assets/instagram.png';
+import swimmingImg from '@/assets/swimming.png';
+import classImg from '@/assets/class.png';
+import playgroundImg from '@/assets/playground.png';
+import bgImage from '@/assets/bg.png';
 
 const RighSidebar = () => {
   const handleGoogleSignin = async () => {
@@ -16,7 +24,20 @@ const RighSidebar = () => {
     });
   };
 
+  const socialPlatforms = [
+    { name: 'Facebook', icon: fbIcon, border: true },
+    { name: 'Twitter', icon: twitterIcon, border: true },
+    { name: 'Instagram', icon: instaIcon, border: false },
+  ];
+
+  const qZoneImages = [
+    { id: 1, img: swimmingImg, alt: 'Swimming' },
+    { id: 2, img: classImg, alt: 'Class' },
+    { id: 3, img: playgroundImg, alt: 'Play Ground' },
+  ];
+
   return (
+    // login with form section
     <div>
       <h2 className="font-semibold text-gray-600 text-lg mb-5">Login With</h2>
 
@@ -35,6 +56,65 @@ const RighSidebar = () => {
           <FaGithub />
           Login With GitHub
         </button>
+      </div>
+
+      {/* social media section */}
+      <div className="mt-7">
+        <h2 className="text-xl font-semibold text-slate-700 mb-5">
+          Find Us On
+        </h2>
+        <div className="flex flex-col border border-gray-200 rounded-lg overflow-hidden bg-white">
+          {socialPlatforms.map((platform, index) => (
+            <button
+              key={index}
+              className={`btn btn-ghost gap-3 flex justify-start items-center p-4 h-auto rounded-none transition-all capitalize font-medium text-slate-600 hover:bg-slate-50 ${
+                platform.border ? 'border-b border-gray-200' : 'border-none'
+              }`}
+            >
+              <span className="bg-slate-100 p-2 rounded-full h-10 w-10 flex items-center justify-center shrink-0">
+                <Image
+                  src={platform.icon}
+                  alt={platform.name}
+                  width={20}
+                  height={20}
+                  className="object-contain"
+                />
+              </span>
+              <span className="text-base">{platform.name}</span>
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {/* Q-Zone section */}
+      <div className="mt-4 bg-gray-100 p-3 rounded-lg">
+        <h2 className="text-xl font-semibold text-slate-700 mb-5">Q-Zone</h2>
+
+        <div className="flex flex-col gap-6 items-center">
+          {qZoneImages.map((item) => (
+            <div key={item.id} className="relative w-full">
+              <Image
+                src={item.img}
+                alt={item.alt}
+                className="w-full h-auto object-contain"
+                style={{
+                  filter: 'drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.1))',
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-6">
+        <Image
+          src={bgImage}
+          alt={bgImage}
+          className="w-full h-auto object-contain rounded-md"
+          style={{
+            filter: 'drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.1))',
+          }}
+        />
       </div>
     </div>
   );
